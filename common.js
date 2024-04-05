3f37e29a430594c48f4a5bf79b588e99106f10fc
chaeng <chaengleee@naver.com>
2024-04-05 11:51:37 +0900

    수정

--- common.js
+++ common.js
@@ -2,4 +2,19 @@ let swiper = new Swiper(".addslide", {
   watchSlidesProgress: true,
   slidesPerView: 3.5,
   loop: true,
+  breakpoints: {
+    800: {
+      slidesPerView: 2.5,
+    },
+    1250: {
+      slidesPerView: 3.5,
+    },
+    500: {
+      slidesPerView: 2.5,
+    },
+  },
 });
+
+// document.querySelector(".ham").addEventListener(click, function () {
+//   const hamMenu = document.querySelector(".hammenu");
+// });
